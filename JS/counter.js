15adbc8900a1fddb9bf9ea202a632479dcd51d8d
Xirade <6myusic6@gmail.com>
2020-04-08 15:09:48 +0300

    Update counter.js

--- JS/counter.js
+++ JS/counter.js
@@ -3,11 +3,28 @@ let commentField = document.querySelector ('.comment-field');
 let charCounter = document.querySelector ('.char-counter');
 let submitButton = document.querySelector ('.submit-button');
 var progressCounter = document.querySelector ('span.progress-counter .text-progress');
+var maxlengthValue = document.getElementById('comment-textarea');
 
-    commentField.oninput = function () { 
+commentField.oninput = function () { 
+    maxlengthValue.getAttribute('maxlength', 'value');
     charCounter.textContent = commentField.value.length;
     progressCounter.style.setProperty('--element-width', charCounter.textContent + '%');
-};
+    if (charCounter.textContent > maxlengthValue.textContent){
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+        $('textarea').keydown(function(e){
+            e.preventDefault();
+        });
+    }
+    };
+
+submitButton.onclick = function (){
+    commentField.value = '';
+    charCounter.textContent = 0;
+}
+
+
 
 
 //getComputedStyle(ссылкаНаЭлемент).getPropertyValue('--моя-переменная').
